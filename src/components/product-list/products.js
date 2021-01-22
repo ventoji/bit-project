ec21bc41ab1a77de3f5da695f6d7baca4228dad7
ventoji <ventoji@gmail.com>
2021-01-22 12:24:54 +0100

    fix: packages peer dependencies

---
+++ src/components/product-list/products.js
@@ -0,0 +1,17 @@
+export const products = [
+    {
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        name: 'Phone Standard 1',
+        price: 299,
+        description: ''
+    }
+];
